e1ba472b1810f5218b210d35526fc28528e74603
BarRaider <46548278+BarRaider@users.noreply.github.com>
2020-04-29 14:37:52 -0700

    Create pickers.js

---
+++ src/pickers.js
@@ -0,0 +1,38 @@
+// ****************************************************************
+// * EasyPI v1.3
+// * Author: BarRaider
+// *
+// * pickers.js adds support for plugin-side pickers.
+// *
+// * Project page: https://github.com/BarRaider/streamdeck-easypi
+// * Support: http://discord.barraider.com
+// ****************************************************************
+
+function openSaveFilePicker(title, filter, propertyName) {	
+    console.log("openSaveFilePicker called: ", title, filter, propertyName);	
+    var payload = {};	
+    payload.property_inspector = 'loadsavepicker';	
+    payload.picker_title = title;	
+    payload.picker_filter = filter;	
+    payload.property_name = propertyName;	
+    sendPayloadToPlugin(payload);	
+}
+
+function openLoadFilePicker(title, filter, propertyName) {	
+    console.log("openLoadFilePicker called: ", title, filter, propertyName);	
+    var payload = {};	
+    payload.property_inspector = 'loadopenpicker';	
+    payload.picker_title = title;	
+    payload.picker_filter = filter;	
+    payload.property_name = propertyName;	
+    sendPayloadToPlugin(payload);	
+}
+
+function openDirectoryPicker(title, propertyName) {
+    console.log("openDirectoryPicker called: ", title, propertyName);
+    var payload = {};
+    payload.property_inspector = 'loadfolderpicker';
+    payload.picker_title = title;
+    payload.property_name = propertyName;
+    sendPayloadToPlugin(payload);
+}
